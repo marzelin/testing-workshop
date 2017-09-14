1447cc9133c0b55c053e31f30a86c86268a1faa9
marzelin <marzelin@gmail.com>
2017-09-14 14:53:26 +0200

    solve challenge 2

--- api/src/routes/utils/__tests__/get-token-from-header.js
+++ api/src/routes/utils/__tests__/get-token-from-header.js
@@ -13,14 +13,38 @@
 // to see how this function has been implemented and
 // how it's intended to be used. Write at least two unit
 // tests to ensure that that use case is always supported.
-import getTokenFromHeader from '../get-token-from-header'
+import getTokenFromHeader from "../get-token-from-header"
 
-test('this is the title of your test', () => {
+describe("get-token-from-header", () => {
+  test("should return null if no authorization header provided", () => {
+    const req = getReq(null)
+    const result = getTokenFromHeader(req)
+    expect(result).toBeNull()
+  })
+  test("should return jwt token if valid authorization header provided", () => {
+    const token = "header.payload.signature"
+    const req = getReq(token)
+    const result = getTokenFromHeader(req)
+    expect(result).toEqual(token)
+  })
   // this is where you put your test code. Write code that will
   // throw an error if getTokenFromHeader has a bug. The `expect`
   // global is a utility that makes writing such assertions easier,
   // but you can do it however you like.
 })
+
+function getReq(token) {
+  if (token == null) {
+    return {
+      headers: {}
+    }
+  }
+  return {
+    headers: {
+      authorization: `Token ${token}`
+    }
+  }
+}
 
 //////// Elaboration & Feedback /////////
 // When you've finished with the exercises:
@@ -31,7 +55,7 @@ test('this is the title of your test', () => {
 /*
 http://ws.kcd.im/?ws=Testing&e=API%20Unit&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
+test.skip("I submitted my elaboration and feedback", () => {
   const submitted = false // change this when you've submitted!
   expect(true).toBe(submitted)
 })
